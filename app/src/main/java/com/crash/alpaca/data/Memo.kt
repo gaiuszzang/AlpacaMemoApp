79b17bad88e4a5b8693b06cfc2ebc2d58d83ae7c
Hyunhae Lee <hyunhae.lee@gmail.com>
2020-05-21 17:40:22 +0000

    [AlpacaMemo] update memo entity
    add columns for memo entity

--- app/src/main/java/com/crash/alpaca/data/Memo.kt
+++ app/src/main/java/com/crash/alpaca/data/Memo.kt
@@ -1,9 +1,25 @@
 package com.crash.alpaca.data
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.ForeignKey
 
-@Entity(tableName = "memo", primaryKeys = ["id"])
+@Entity(
+    tableName = "memo",
+    primaryKeys = ["id"],
+    foreignKeys = [
+        ForeignKey(
+            entity = MemoRoom::class,
+            parentColumns = ["id"],
+            childColumns = ["room_id"],
+            onDelete = ForeignKey.CASCADE
+        )
+    ]
+)
 data class Memo(
     val id: String,
-    val content: String
+    @ColumnInfo(name = "room_id")
+    val roomId: Int,
+    val content: String,
+    val time: Long
 )
